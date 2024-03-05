7258377a417820659a4e464343e8eca3752d4d3e
Clinton Raj <clintonraj007@outlook.com>
2024-03-05 09:14:23 +0530

    warning updates

--- test_Appium_Windows_HP_Smart.py
+++ test_Appium_Windows_HP_Smart.py
@@ -9,16 +9,16 @@ from appium.webdriver.appium_service import AppiumService
 from selenium.webdriver.support import expected_conditions as EC
 
 def setup_function(function):
-    # global appium_Service  # coonect to the appium server without open Appium GUI
+    # global appium_Service  # connect to the appium server without open Appium GUI
     # appium_Service= AppiumService()
     # appium_Service.start()
 
     global driver
-    cap: Dict[str, Any]={
+    cap: Dict[str, Any] = {
         'platformName': 'Windows',
         'app': 'AD2F1837.HPPrinterControl_v10z8vjag6ke6!AD2F1837.HPPrinterControl'
     }
-    url= 'http://127.0.0.1:4723/wd/hub'
+    url = 'http://127.0.0.1:4723/wd/hub'
     driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
     driver.implicitly_wait(30)
     driver.maximize_window()
@@ -26,7 +26,7 @@ def setup_function(function):
 
 def tear_down(function):
     driver.quit()
-    # appium_Service.stop()  # coonect to the appium server without open Appium GUI
+    # appium_Service.stop()  # connect to the appium server without open Appium GUI
 
 def test_demo():
     print("pass")
